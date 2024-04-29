b827c02d6a852855348045271f9ee75448e26266
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2024-04-29 12:39:15 -0400

    Added hasRead status and more functionalities Ability to close the popup and error messages

--- main.js
+++ main.js
@@ -1,34 +1,55 @@
 const showPopup = document.querySelector(".show-popup");
 const popupContainer = document.querySelector(".popup-container");
+const submitBtn = document.querySelector(".submit-btn");
 const closeBtn = document.querySelector(".close-btn");
 const bookshelf = document.querySelector(".bookshelf");
+const errorMessage = document.querySelector(".errorMessage");
 
 showPopup.onclick = () => {
   popupContainer.classList.add("active");
 };
 
 closeBtn.onclick = () => {
+  popupContainer.classList.remove("active");
+};
+
+submitBtn.onclick = () => {
   const book = Array.from(document.querySelectorAll("#book-form input")).reduce(
     (acc, input) => ({ ...acc, [input.id]: input.value }),
     {}
   );
 
+  if (
+    book.Author.length === 0 ||
+    book.title.length === 0 ||
+    book.Pages.length === 0
+  ) {
+    errorMessage.innerText = "Please input the required information.";
+    return;
+  } else {
+    errorMessage.innerText = "";
+  }
   console.log(book);
-  addBookToLibrary(book.Author, book.title, book.Pages);
+  addBookToLibrary(book.Author, book.title, book.Pages, book.hasRead);
+
+  document.querySelectorAll("#book-form input").forEach((input) => {
+    input.value = ""; // Clear the value of each input field
+  });
 
   popupContainer.classList.remove("active");
 };
 
 const myLibrary = [];
 
-function Book(author, title, page) {
+function Book(author, title, page, hasRead) {
   this.author = author;
   this.title = title;
   this.page = page;
+  this.hasRead = hasRead;
 }
 
-function addBookToLibrary(author, title, page) {
-  const book = new Book(author, title, page);
+function addBookToLibrary(author, title, page, hasRead) {
+  const book = new Book(author, title, page, hasRead);
   myLibrary.push(book);
   createBookCard(book);
 }
@@ -45,6 +66,11 @@ function createBookCard(book) {
 
   const pageLabel = document.createElement("label");
   const page = document.createElement("input");
+
+  const hasReadLabel = document.createElement("label");
+  const switchLabel = document.createElement("label");
+  const switchSpan = document.createElement("span");
+  const hasReadInput = document.createElement("input");
 
   titleLabel.setAttribute("for", "title");
   titleLabel.innerText = "Title:";
@@ -67,11 +93,23 @@ function createBookCard(book) {
   page.setAttribute("value", `${book.page}`);
   page.disabled = true;
 
+  hasReadLabel.setAttribute("for", "hasRead");
+  hasReadLabel.innerText = "Read Status:";
+  switchLabel.classList.add("switch");
+  switchLabel.appendChild(hasReadInput);
+  switchLabel.appendChild(switchSpan);
+  switchSpan.classList.add("slider", "round");
+  hasReadInput.setAttribute("type", "checkbox");
+  hasReadInput.setAttribute("id", "hasRead");
+  hasReadInput.checked = book.hasRead === "on";
+
   bookCard.appendChild(titleLabel);
   bookCard.appendChild(title);
   bookCard.appendChild(authorLabel);
   bookCard.appendChild(author);
   bookCard.appendChild(pageLabel);
   bookCard.appendChild(page);
+  bookCard.appendChild(hasReadLabel);
+  bookCard.appendChild(switchLabel);
   bookshelf.appendChild(bookCard);
 }
